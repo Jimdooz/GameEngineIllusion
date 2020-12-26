afed8cc77824f79ad46e36d3eff69188d1252b71
jimdooz <romain.saclier@gmail.com>
2020-12-26 20:31:04 +0100

    Temp Rendering

--- Illusion/IllusionEditor/src/IllusionEditor.cpp
+++ Illusion/IllusionEditor/src/IllusionEditor.cpp
@@ -38,6 +38,9 @@ namespace fs = std::filesystem;
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
+//TEMP RENDERING
+#include "tmp/Shader.h"
+
 using namespace std::chrono;
 using namespace illusion;
 using namespace illusioneditor;
@@ -130,6 +133,71 @@ int main(int argc, char* argv[]) {
 	std::string pathName = currentPath.path().string();
 	std::string projectName = "";
 
+	// Temp rendering
+	//---------
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// texture coord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	Shader ourShader("D:\\GitHub\\GameEngineIllusion\\GameProjects\\Optimulus\\Assets\\Shader\\vertexShader.glsl",
+					"D:\\GitHub\\GameEngineIllusion\\GameProjects\\Optimulus\\Assets\\Shader\\fragmentShader.glsl");
+	ourShader.use();
 	// Main Loop
 	//---------
 	while (!Window::shouldClose) {
@@ -299,15 +367,48 @@ int main(int argc, char* argv[]) {
 		views::GameProject::SetScene(scene);
 		views::GameProject::Show();
 
-		//Render
+		//RENDER
+		//--------
 		ImGui::Render();
 		int display_w, display_h;
 		glfwGetFramebufferSize(Window::glfwWindow, &display_w, &display_h);
 		glViewport(0, 0, display_w, display_h);
 		glClearColor(0.25f, 0.25f, 0.5f, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		//DRAW RENDERING
+		{
+			// activate shader
+			ourShader.use();
+
+			// create transformations
+			glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+			glm::mat4 projection = glm::mat4(1.0f);
+			projection = glm::perspective(glm::radians(45.0f), (float)Window::width / (float)Window::height, 0.1f, 100.0f);
+			view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+			// pass transformation matrices to the shader
+			ourShader.setMat4("projection", projection); // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+			ourShader.setMat4("view", view);
+
+			// render boxes
+			glBindVertexArray(VAO);
+			ecs::core::Transform &transform = *scene.GetComponent<ecs::core::Transform>();
+			for (u32 i = 0; i < transform.ToEntity.size(); i++) {
+				// calculate the model matrix for each object and pass it to shader before drawing
+				glm::mat4 model = glm::mat4(1.0f);
+				model = glm::translate(model, transform.position[i]);
+				model = model * glm::toMat4(transform.rotation[i]);
+				model = glm::scale(model, transform.scale[i]);
+				ourShader.setMat4("model", model);
+
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+			}
+		}
+
+		//DRAW IMGUI
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
+		//UPDATE
 		auto start = high_resolution_clock::now();
 		scene.Update();
 		auto stop = high_resolution_clock::now();
@@ -315,6 +416,7 @@ int main(int argc, char* argv[]) {
 		fpsMesure.push_back(duration > 0 ? duration : 1);
 		if (fpsMesure.size() > 100) fpsMesure.erase(fpsMesure.begin());
 
+		//SWAP BUFFER
 		glfwSwapBuffers(Window::glfwWindow);
 	}
 
