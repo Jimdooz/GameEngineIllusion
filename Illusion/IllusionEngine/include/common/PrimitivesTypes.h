78efd46ddfbfdd9cc2dde7e0e6ae04fbfbb08c62
jimdooz <romain.saclier@gmail.com>
2020-12-28 12:15:00 +0100

    Declaration Order Correction

--- Illusion/IllusionEngine/include/common/PrimitivesTypes.h
+++ Illusion/IllusionEngine/include/common/PrimitivesTypes.h
@@ -4,11 +4,6 @@
 	/!\ DO NOT INCLUDE THIS FILE, INCLUDE "CommonHeader.h" INSTEAD
 */
 
-constexpr f32 PI = 3.14159265358979323846;
-constexpr f32 EPSILON = std::numeric_limits<double>::epsilon();
-
-constexpr bool NEAR_EPSILON(f32 v) { return v < EPSILON || v > -EPSILON; }
-
 //unsigned integers
 using u8 = uint8_t;
 using u16 = uint16_t;
@@ -34,3 +29,8 @@ using Quaternion = glm::quat;
 using Mat2x2 = glm::mat2x2;
 using Mat3x3 = glm::mat3x3;
 using Mat4x4 = glm::mat4x4;
+
+static constexpr f32 const& PI = 3.14159265358979323846;
+static constexpr f32 const& EPSILON = std::numeric_limits<double>::epsilon();
+
+constexpr bool NEAR_EPSILON(f32 v) { return v < EPSILON || v > -EPSILON; }
