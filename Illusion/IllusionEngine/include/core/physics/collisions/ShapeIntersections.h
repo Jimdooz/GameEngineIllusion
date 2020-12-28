427c611d060757c753416a3146eb56b6f79d5770
jimdooz <romain.saclier@gmail.com>
2020-12-28 12:37:21 +0100

    Add Shape collisions 2

--- Illusion/IllusionEngine/include/core/physics/collisions/ShapeIntersections.h
+++ Illusion/IllusionEngine/include/core/physics/collisions/ShapeIntersections.h
@@ -11,18 +11,22 @@ namespace illusion::core::physics::collisions {
 	 *		Tests dones : 
 	 *
 	 *					Sphere	AABB	OBB		Plane
-	 *		Sphere		x		x		-		-
-	 *		AABB		-		-		-		-
-	 *		OBB			-		-		-		-
-	 *		Plane		-		-		-		-
+	 *		Sphere		x		x		x		x
+	 *		AABB		x		x		-		-
+	 *		OBB			x		-		-		-
+	 *		Plane		x		-		-		-
 	 * 
 	 */
 
 	bool SphereSphere(const Sphere& s1, const Sphere& s2);
 	bool SphereAABB(const Sphere& sphere, const AABB& aabb);
 	bool SphereOBB(const Sphere& sphere, const OBB& obb);
+	bool SpherePlane(const Sphere& sphere, const Plane& plane);
+
+	bool AABBAABB(const AABB& aabb1, const AABB& aabb2);
 
 	// Mirror Call
 	constexpr bool AABBSphere(const AABB& aabb, const Sphere& sphere) { return SphereAABB(sphere, aabb); }
-	constexpr bool SphereOBB(const OBB& obb, const Sphere& sphere);
+	constexpr bool SphereOBB(const OBB& obb, const Sphere& sphere) { return SphereOBB(obb, sphere); }
+	constexpr bool SpherePlane(const Plane& plane, const Sphere& sphere) { return SpherePlane(sphere, plane); }
 }
--- Illusion/IllusionEngine/src/core/physics/collisions/ShapeIntersections.cpp
+++ Illusion/IllusionEngine/src/core/physics/collisions/ShapeIntersections.cpp
@@ -12,9 +12,33 @@ namespace illusion::core::physics::collisions {
 
 	bool SphereAABB(const Sphere& sphere, const AABB& aabb) {
 		Point closestPoint = ClosestPoint(aabb, sphere.position);
+		f32 distSq = glm::length2(sphere.position - closestPoint);
+		f32 radiusSq = sphere.radius * sphere.radius;
+		return distSq < radiusSq;
+	}
+
+	bool SphereOBB(const Sphere& sphere, const OBB& obb) {
+		Point closestPoint = ClosestPoint(obb, sphere.position);
+		f32 distSq = glm::length2(sphere.position - closestPoint);
+		f32 radiusSq = sphere.radius * sphere.radius;
+		return distSq < radiusSq;
+	}
+
+	bool SpherePlane(const Sphere& sphere, const Plane& plane) {
+		Point closestPoint = ClosestPoint(plane, sphere.position);
 		float distSq = glm::length2(sphere.position - closestPoint);
 		float radiusSq = sphere.radius * sphere.radius;
 		return distSq < radiusSq;
 	}
 
+	bool AABBAABB(const AABB& aabb1, const AABB& aabb2) {
+		Point aMin = AABB::GetMin(aabb1);
+		Point aMax = AABB::GetMax(aabb1);
+		Point bMin = AABB::GetMin(aabb2);
+		Point bMax = AABB::GetMax(aabb2);
+
+		return (aMin.x <= bMax.x && aMax.x >= bMin.x) &&
+			(aMin.y <= bMax.y && aMax.y >= bMin.y) &&
+			(aMin.z <= bMax.z && aMax.z >= bMin.z);
+	}
 }
